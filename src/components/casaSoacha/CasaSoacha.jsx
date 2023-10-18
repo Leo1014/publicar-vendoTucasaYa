be68723f424d92f65c866f6c07efbb249cc2ad24
Leo1014 <las1014@gmail.com>
2023-10-18 16:41:11 -0500

    anuncio

--- src/components/casaSoacha/CasaSoacha.jsx
+++ src/components/casaSoacha/CasaSoacha.jsx
@@ -3,13 +3,15 @@ import "./casaSoacha.css"
 
 export default function CasaSoacha(){
     return(
-        <div>
+        <div className="container">
             <h1>Casa de Tres Pisos Totalmente Terminada</h1>
             <h2>Garaje automovil - patio de ropa con teja corrediza</h2>
-            <p>Primer piso: tres alcobas - Baño - Cocina - Patio de ropas teja corrediza</p>
-            <p>Salon de 5.50 x 5.00 metros</p>
-            <p>Segundo Piso: tres alcobas - cocina - comedor - baño</p>
-            <p>Tercer Piso: apartamento - cocina - comedor - baño - Patio Grande con Marqueteria</p>
+            <div className="description">
+                <p>Primer piso: tres alcobas - Baño - Cocina - Patio de ropas teja corrediza</p>
+                <p>Salon de 5.50 x 5.00 metros</p>
+                <p>Segundo Piso: tres alcobas - cocina - comedor - baño</p>
+                <p>Tercer Piso: apartamento - cocina - comedor - baño - Patio Grande con Marqueteria</p>
+            </div>
         </div>
     )
 }
